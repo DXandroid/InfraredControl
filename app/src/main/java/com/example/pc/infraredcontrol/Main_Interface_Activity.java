6453e385e8923b59e170f3fa324d953a6ea87a66
fengsen <799144109@qq.com>
2018-10-13 15:43:45 +0800

    动画2

--- app/src/main/java/com/example/pc/infraredcontrol/Main_Interface_Activity.java
+++ app/src/main/java/com/example/pc/infraredcontrol/Main_Interface_Activity.java
@@ -1,13 +1,18 @@
 package com.example.pc.infraredcontrol;
 
 import android.app.WallpaperManager;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.hardware.ConsumerIrManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.view.MotionEvent;
 
 public class Main_Interface_Activity extends AppCompatActivity {
 
@@ -24,7 +29,29 @@ public class Main_Interface_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main_interface);
         intiset_button();
         //加载第一份动画效果
-       // final Animation anim = AnimationUtils;
+        final Animation anim = AnimationUtils.loadAnimation(this,R.anim.anim);
+        //效果结束后保留动画资源
+        anim.setFillAfter(true);
+        //加载第二份动画
+        final Animation reverse = AnimationUtils.loadAnimation(this,R.anim.reverse);
+        //效果结束后保留动画资源
+        anim.setFillAfter(true);
+        //设定监听
+        button_power.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    button_power.startAnimation(anim);
+                    button_power.setBackgroundColor(Color.GRAY);
+                }
+                else if(motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    button_power.startAnimation(reverse);
+                    button_power.setBackgroundColor(Color.WHITE);
+                }
+                return false;
+            }
+        });
+
         //把桌面背景设置为activity背景
         WallpaperManager  manager=WallpaperManager.getInstance(this);
         Drawable drawable=manager.getDrawable();
@@ -35,6 +62,7 @@ public class Main_Interface_Activity extends AppCompatActivity {
         can_infrared=false;
         in_con=new Infrared_Control((ConsumerIrManager) getSystemService(CONSUMER_IR_SERVICE));
     }
+
 
     private void intiset_button(){
         button_power=findViewById(R.id.power);
@@ -47,8 +75,8 @@ public class Main_Interface_Activity extends AppCompatActivity {
 
     protected void onStart() {
         super.onStart();
-        if(in_con.equipment_judge_useful())
-            set_button_useful();
+     //   if(in_con.equipment_judge_useful())
+           set_button_useful();
     }
     /**
      * 当设备有红外可用时才使所有按键有效
@@ -57,7 +85,6 @@ public class Main_Interface_Activity extends AppCompatActivity {
         can_infrared=true;
         button_power.setEnabled(true);
     }
-
     public void button_power_onclick(View view){
         in_con.set_power();
         //in_con.get_mag(t);
