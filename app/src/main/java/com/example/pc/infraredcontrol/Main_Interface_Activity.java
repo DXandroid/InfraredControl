86e842693e487944a9f21aa031d6ad317ea8ff87
inazhangqi <345914751@qq.com>
2018-10-13 11:57:12 +0800

    Revert "界面1"
    This reverts commit 74c8a34

--- app/src/main/java/com/example/pc/infraredcontrol/Main_Interface_Activity.java
+++ app/src/main/java/com/example/pc/infraredcontrol/Main_Interface_Activity.java
@@ -1,5 +1,7 @@
 package com.example.pc.infraredcontrol;
 
+import android.app.WallpaperManager;
+import android.graphics.drawable.Drawable;
 import android.hardware.ConsumerIrManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,7 +14,7 @@ public class Main_Interface_Activity extends AppCompatActivity {
     Infrared_Control in_con;
     boolean can_infrared;
 
-    Button button_power;
+    Button button_power,button_add,button_minus;
 
     TextView t;
 
@@ -21,6 +23,10 @@ public class Main_Interface_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_interface);
         intiset_button();
+        //设置背景
+        WallpaperManager wallpaperManager=WallpaperManager.getInstance(this);
+        Drawable drawable=wallpaperManager.getDrawable();
+        this.getWindow().setBackgroundDrawable(drawable);
 
         t=findViewById(R.id.get);
 
@@ -31,6 +37,10 @@ public class Main_Interface_Activity extends AppCompatActivity {
     private void intiset_button(){
         button_power=findViewById(R.id.power);
         button_power.setEnabled(false);
+        button_add=findViewById(R.id.add);
+        button_add.setEnabled(false);
+        button_minus=findViewById(R.id.minus);
+        button_minus.setEnabled(false);
     }
 
     protected void onStart() {
@@ -50,8 +60,12 @@ public class Main_Interface_Activity extends AppCompatActivity {
         in_con.set_power();
         //in_con.get_mag(t);
     }
+    public void button_add_onclick(View view){
 
+    }
     //github test
+    public void button_minus_onclick(View view){
 
+    }
 
 }
