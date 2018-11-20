42b3b6720d48a5ca11ee8b25385377e013a6cd96
fengsen <799144109@qq.com>
2018-11-20 14:45:20 +0800

    学习初版

---
+++ app/src/main/java/com/example/pc/infraredcontrol/Button_Learn.java
@@ -0,0 +1,13 @@
+package com.example.pc.infraredcontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Button_Learn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button__learn);
+    }
+}
