e6ae1fa6c359063b3bbff78d5269513bbf3f1c16
fengsen <799144109@qq.com>
2018-11-20 20:21:48 +0800

    学习初版3.0

--- app/src/main/java/com/example/pc/infraredcontrol/Button_Learn.java
+++ app/src/main/java/com/example/pc/infraredcontrol/Button_Learn.java
@@ -1,11 +1,13 @@
 package com.example.pc.infraredcontrol;
 
+import android.app.Activity;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class Button_Learn extends AppCompatActivity {
+public class Button_Learn extends AppCompatActivity implements View.OnClickListener{
 
     EventProcessing EP;
     Button back, cancel, save;
@@ -16,9 +18,11 @@ public class Button_Learn extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button__learn);
         EP = (EventProcessing) getApplication();
-        back = findViewById(R.id.back);
+
         cancel = findViewById(R.id.cancel);
         save = findViewById(R.id.save);
+        back = findViewById(R.id.back);
+        back.setOnClickListener(this);
 
     }
 
@@ -26,6 +30,7 @@ public class Button_Learn extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         setButton();
+
     }
 
     private void setButton(){
@@ -38,5 +43,11 @@ public class Button_Learn extends AppCompatActivity {
             save.setBackgroundColor(Color.parseColor("#f34649"));
         }
     }
-
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.back:
+                this.finish();
+                break;
+        }
+    }
 }
